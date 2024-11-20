f171c9d2a8592d31857f8adbc2cb252025dd8a27
drunkentaco <134489107+Drunkentaco@users.noreply.github.com>
2024-11-20 11:29:55 -0500

    fnished excerise 5 and 6, they were a lot easier than 4 :D

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+   const numbers = [num1, num2]
+    if (
+    (Number.isInteger(num1) === false)||
+    (Number.isInteger (num2) === false)
+    ){
+        return "ERROR";
+    }else if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    } else
+    numbers.sort();
+    let end = numbers.pop();
+    let start = numbers.shift();
+    
+    let sum = 0;
+    for (let n = start; n <= end; n++){
+        sum += n
+    }
+return sum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
